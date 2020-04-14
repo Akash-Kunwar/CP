ff1e29e3f7b9e952c1867a796a56c2bfa6968d6e
Akash Kunwar <akashkunwar08@gmail.com>
2020-04-15 00:05:00 +0530

    Add solution for problem `C - Two Teams Composing` Link: https://codeforces.com/contest/1335/problem/C Tags: binary search, greedy, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1335/submission/76728807

---
+++ codeforces/1335/C.py
@@ -0,0 +1,14 @@
+for i in range(int(input())):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    d={}
+    maxcount=0
+    for i in range(len(arr)):
+        if arr[i] in d.keys():
+            d[arr[i]]+=1
+        else:
+            d[arr[i]]=1
+        maxcount=max(maxcount,d[arr[i]])
+    diff=len(d)
+    print(max(min(diff-1,maxcount),min(maxcount-1,diff)))
+    
