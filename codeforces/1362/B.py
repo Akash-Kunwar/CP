8f610e59a3e76bbc32379b0c6ac43e46b2065b15
Akash Kunwar <akashkunwar08@gmail.com>
2020-06-05 10:34:00 +0530

    Add solution for problem `B - Johnny and His Hobbies` Link: https://codeforces.com/contest/1362/problem/B Tags: bitmasks, brute force, *1200 Ref: https://codeforces.com/contest/1362/submission/82596067

--- codeforces/1362/B.py
+++ codeforces/1362/B.py
@@ -1,13 +1,14 @@
 for _ in range(int(input())):
     n=int(input())
     arr=list(map(int,input().split()))
-    flag =  -1  
+    flag =  -1
+    s=set(arr)
     for i in range(1,1024):
         a=[]
-        for j in arr:
+        for j in s:
             a.append(i^j)
-        # print(a)
-        if(set(arr)==set(a)):
+        s1=set(a)
+        if(s1==s):
             flag=i
             break
     print(flag)
