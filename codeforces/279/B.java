848f421eb6cdd9c116b65650784bc188e57c3bef
Akash Kunwar <akashkunwar08@gmail.com>
2020-06-24 22:49:00 +0530

    Add solution for problem `B - Books` Link: https://codeforces.com/contest/279/problem/B Tags: binary search, brute force, implementation, two pointers, *1400 Ref: https://codeforces.com/contest/279/submission/84912858

--- codeforces/279/B.java
+++ codeforces/279/B.java
@@ -1,47 +1,112 @@
 import java.util.*;
-import java.io.*;
 import java.lang.*;
-public class Main{ 
-    public static void main(String[] args) {
-        FastScanner sc =new FastScanner();
-        PrintWriter out=new PrintWriter(System.out);
-        int n=sc.nextInt(),j=0,max=0;
-        long k=sc.nextLong();
-        int arr[] =new int[n];
-        for(int i=0;i<n;i++) arr[i]=sc.nextInt();
-        long temp=0;
+import java.io.*;
 
-        for(int i=0;i<n;i++){
-            while(j<n && temp+arr[j]<=k){
-                temp+=arr[j];
-                j++;
-            }
-            // out.println(j+" "+i);
-            max=Math.max(j-i,max);
-            temp-=arr[i];
-        }
-        out.println(max);
-        out.close();
-    }
-}
+public class GFG{
+   public static void main (String[] args)  throws Exception
+	 {
+      FastScanner sc=new FastScanner(System.in);
+      int n=sc.nextInt();
+      int m=sc.nextInt();
+      int arr[]=new int [n];
 
-class FastScanner{
-    BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st =new StringTokenizer("");
-    String next(){
-        if(!st.hasMoreTokens()){
-            try{
-                st=new StringTokenizer(br.readLine());
-            }
-            catch(Exception e){    
-            }
-        }
-        return st.nextToken();
-    }
-    int nextInt(){
-        return Integer.parseInt(next());
-    }
-    long nextLong(){
-        return Long.parseLong(next());
-    }
-}
+      for(int i=0;i<n;i++){
+         arr[i]=sc.nextInt();
+      }
+
+      int count=0;
+      int i=0,j=0;
+      int sum=0;
+      while(i<n){
+
+         while(j<n && sum+arr[j]<=m){
+            sum+=arr[j];
+            j++;
+         }
+         count=Math.max(count,j-i);
+         sum-=arr[i];
+         i++;
+      }
+      System.out.println(count);
+      
+   }
+}	
+
+class FastScanner
+{
+   
+   final private int BUFFER_SIZE = 1 << 17;
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+ 
+   public FastScanner(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+   public String nextLine() throws Exception
+   {
+	   StringBuffer sb=new StringBuffer("");
+	   byte c = read();
+	   while (c <= ' ') c = read();
+	   do
+	   {
+		   sb.append((char)c);
+		   c=read();
+	   }while(c>' ');
+	   return sb.toString();
+   }
+
+   public char nextChar() throws Exception
+   {
+	   byte c=read();
+	   while(c<=' ') c= read();
+	   return (char)c;
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+    	  ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+    	   ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+ 
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+} 
