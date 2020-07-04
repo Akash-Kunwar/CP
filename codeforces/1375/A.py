e9d54c4828f26565ac8fa86ec6349be2b75c513c
Akash Kunwar <akashkunwar08@gmail.com>
2020-07-04 21:37:00 +0530

    Add solution for problem `A - Sign Flipping` Link: https://codeforces.com/contest/1375/problem/A Tags: constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1375/submission/85973436

--- codeforces/1375/A.py
+++ codeforces/1375/A.py
@@ -1,11 +1,42 @@
 for _ in range(int(input())):
     n=int(input())
-    arr=list(map(int,input().split()))
 
+    arr=list(map(int,input().split()))
+    pos=0
+    neg=0
+    zero=0
+    i=1
     for i in range(n):
-        if(i%2!=0):
-            arr[i]=abs(arr[i])
-        else:
-            arr[i]=-abs(arr[i])
+        arr[i]=abs(arr[i])
+    for i in range(1,n-1,2):
+
+        a=arr[i-1]
+        b=arr[i]
+        c=arr[i+1]
+
+        if(b>=a and b>=c):
+            pass
+        elif(b<=a and b<=c):
+            pass
+        elif(b<=a and b>=c):
+            b=-b
+            c=-c
+        elif(b>=a and b<=c):
+            c=-c
+        arr[i-1]=a
+        arr[i]=b
+        arr[i+1]=c
+
+
+
+
     
+    for i in range(1,len(arr)):
+        if(arr[i]-arr[i-1]>0):
+            pos+=1
+        elif(arr[i]-arr[i-1]<0):
+            neg+=1
+        else:
+            zero+=1
     print(*arr)
+    # print([pos,neg,zero])
