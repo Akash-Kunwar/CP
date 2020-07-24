47f0aa6b7f93ec0c4552bd29cd6f381f9dd6ac91
Akash Kunwar <akashkunwar08@gmail.com>
2020-07-25 00:16:00 +0530

    Add solution for problem `A - Common Prefixes` Link: https://codeforces.com/contest/1384/problem/A Tags: constructive algorithms, greedy, strings, *1200 Ref: https://codeforces.com/contest/1384/submission/87935348

--- codeforces/1384/A.py
+++ codeforces/1384/A.py
@@ -1,15 +1,24 @@
 for _ in range(int(input())):
     n=int(input())
     arr=list(map(int,input().split()))
+    
     s=''
-    for i in range(26):
-        s+=chr(97+i)
-    s+=s
+    for i in range(52):
+        s+='a'
     ans=[]
     ans.append(s)
     for i in range(n):
-        s=list(s)
-        s[arr[i]]=chr((ord(ans[-1][arr[i]])-97+1)%26+97)
+        s=list(ans[-1])
+        
+        if(s[arr[i]]=='b'):
+            for j in range(arr[i],52):
+                s[j]='a'
+        else:
+            for j in range(arr[i],52):
+                s[j]='b'
+
+
+
         ans.append(''.join(s))
     for i in ans:
         print(i)
