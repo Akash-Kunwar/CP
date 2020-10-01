501607fc88a06118be619f5814fc5fc834776178
Akash Kunwar <akashkunwar08@gmail.com>
2020-10-01 10:13:00 +0530

    Add solution for problem `B - Coins` Link: https://codeforces.com/contest/58/problem/B Tags: greedy, *1300 Ref: https://codeforces.com/contest/58/submission/94373983

---
+++ codeforces/58/B.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main{ 
+    public static void main(String[] args) {
+        FastScanner sc =new FastScanner();
+        PrintWriter out=new PrintWriter(System.out);
+        int a=sc.nextInt();
+        for(int i=a;i>=1;i--){
+            if(a%i==0){
+                a=i;
+                out.print(a+" ");
+            }
+        }
+        out.close();
+    }
+}
+
+class FastScanner{
+    BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st =new StringTokenizer("");
+    String next(){
+        if(!st.hasMoreTokens()){
+            try{
+                st=new StringTokenizer(br.readLine());
+            }
+            catch(Exception e){    
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt(){
+        return Integer.parseInt(next());
+    }
+}
