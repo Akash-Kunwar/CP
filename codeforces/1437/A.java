a2095689a89c081217a0a7e77f03381d979f18fa
Akash Kunwar <akashkunwar08@gmail.com>
2020-11-05 21:09:00 +0530

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/97719717

---
+++ codeforces/1437/A.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Codeforces{
+    BufferedReader br =new BufferedReader(new InputStreamReader(System.in)); 
+    PrintWriter out =new PrintWriter(System.out);
+    StringTokenizer st =new StringTokenizer("");
+    String next(){
+        if(!st.hasMoreTokens()){
+            try{
+                st=new StringTokenizer(br.readLine());
+            }
+            catch(Exception e){
+
+            }
+        }
+        return st.nextToken();
+    }
+    int nextInt(){
+        return Integer.parseInt(next());
+    }
+    long nextLong(){
+        return Long.parseLong(next());
+    }
+    public static void main(String[] args) {
+        new Codeforces().solve();
+    }
+    void solve(){ 
+        int t=nextInt();
+        while(t-->0){
+            long n =nextLong(),m=nextLong();
+            if(2*n>m)out.println("YES");
+            else out.println("NO");
+        }
+        out.close();
+    }
+}
